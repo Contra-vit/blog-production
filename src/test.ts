fd5a4a7a41ee76f808209ca7663f17f2a21ef712
Vladimir Tsaran <vladimirtsaran@MacBook-Air-Vladimir.local>
2023-02-05 20:57:40 +0500

    BuildWebServer

--- src/test.ts
+++ src/test.ts
@@ -1,4 +1,4 @@
 export function someFn(arg: number){
-    console.log('RANDOME FUNCTION new final 1 lesson')
+    console.log('RANDOME FUNCTION test 1')
     return ''
 }
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,39 +1,24 @@
-// const path = require('path');
-// const HTMLWebpackPlugin = require('html-webpack-plugin');
-// const webpack = require('webpack');
 
-import path from 'path';
 import webpack from 'webpack';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
+import {buildWebpackConfig} from "./config/build/buildWebpackConfig";
+import {BuildPaths} from "./config/build/types/config";
+import path from "path";
 
+const paths: BuildPaths ={
+   entry:  path.resolve(__dirname, 'src', 'index.ts'),
+    build: path.resolve(__dirname, 'build'),
+    html:  path.resolve(__dirname, 'public', 'index.html'),
 
-const config: webpack.Configuration = {
-    mode: "development",
-    entry: path.resolve(__dirname, 'src', 'index.ts'),
-    output: {
-        filename: '[name].[contenthash].js',
-        path: path.resolve(__dirname, 'build'),
-        clean: true,
-    },
-    plugins: [
-        new HtmlWebpackPlugin ({
-                template: path.resolve(__dirname, 'public', 'index.html'),
-            }
-        ),
-        new webpack.ProgressPlugin(),
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-        ],
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    },
 }
+const mode = 'development';
+const isDev = mode === 'development';
+const PORT =3000;
+
+const config: webpack.Configuration = buildWebpackConfig({
+    mode: 'development',
+    paths,
+    isDev,
+    port:PORT,
+})
 
 export default config;
