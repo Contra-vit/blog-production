0e11aace4435335bbaf58105107c9d3f894f0be1
vladimirtsaran <vladimir.tsaran.x@gmail.com>
2023-02-07 16:30:27 +0500

    b8 gitignore added

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './index.scss'
 const App = () => {
     return (
         <div className='app'>
-            APPPPPPPPppp
+           New 8 gitignore
             <Counter/>
         </div>
     );
