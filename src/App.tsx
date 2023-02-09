8a2fb57d223587c64c4cee00c3f3852aea604147
vladimirtsaran <vladimir.tsaran.x@gmail.com>
2023-02-09 12:46:20 +0500

    CSS global variables ang theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
-import React from 'react';
-import {Counter} from "./components/Counter";
-import './index.scss'
+import React, {Suspense, useState} from 'react';
+import './styles/index.scss';
+import {Link, Route, Routes} from "react-router-dom";
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
+import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+
+export enum  Theme {
+    LIGHT = 'light',
+    DARK = 'dark',
+
+}
+
 const App = () => {
+    const[theme, setTheme] = useState<Theme>(Theme.LIGHT)
+
+    const toggleTheme = () => {
+        setTheme(theme === Theme.DARK ?  Theme.LIGHT : Theme.DARK)
+
+    }
+
     return (
-        <div className='app'>
-           New 8 gitignore
-            <Counter/>
+        <div className={`app ${theme}`}>
+            <button style={{color: "red", margin: '10px'}} onClick={toggleTheme}>TOGGLE</button>
+             <Link style={{  margin: '10px'}} to={'/'}>Main </Link>
+            <Link to={'/about'} >About </Link>
+            <Suspense fallback={null}>
+            <Routes>
+                <Route  path={'/about'} element={<AboutPageAsync />}/>
+                <Route  path={'/'} element={<MainPageAsync />}/>
+            </Routes>
+            </Suspense>
         </div>
     );
 };
---
+++ src/pages/MainPage/MainPage.async.tsx
@@ -0,0 +1,7 @@
+import {lazy} from "react";
+
+
+// @ts-ignore
+export const MainPageAsync = lazy(() => new Promise(resolve => {
+    setTimeout(() => resolve(import(('./MainPage'))), 2000 )
+}));
