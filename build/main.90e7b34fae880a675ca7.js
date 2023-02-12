aa3c1f85186446ab9333ad006c0c6ae32082dce1
vladimirtsaran <vladimir.tsaran.x@gmail.com>
2023-02-12 22:13:36 +0500

    b12 NavBar

--- build/main.90e7b34fae880a675ca7.js
+++ build/main.90e7b34fae880a675ca7.js
@@ -28147,7 +28147,7 @@ function useInRouterContext() {
  * browsers.
  *
  * Note: If you're using this it may mean you're doing some of your own
- * "routing" in your app, and we'd like to know what your use case is. We may
+ * "router" in your app, and we'd like to know what your use case is. We may
  * be able to provide something higher-level to better suit your needs.
  *
  * @see https://reactrouter.com/docs/en/v6/api#uselocation
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,26 +1,23 @@
 import React, {Suspense} from 'react';
 import './styles/index.scss';
 import {Link, Route, Routes} from "react-router-dom";
-import {className} from "shared/lib/classNames/className";
+import {classNames} from "shared/lib/classNames/classNames";
 import {useTheme} from "app/providers/ThemeProvider";
 import {AboutPage} from "pages/AboutPage";
 import {MainPage} from "pages/MainPage";
+import {AppRouter} from "app/providers/router";
+import {Navbar} from "widgets/NavBar";
 
 
 
 const App = () => {
     const { theme, toggleTheme } = useTheme();
     return (
-        <div className={className('app', {}, [theme])}>
+        <div className={classNames('app', {}, [theme])}>
+         <Navbar/>
+            <AppRouter/>
             <button style={{color: "red", margin: '10px'}} onClick={toggleTheme}>TOGGLE</button>
-             <Link style={{  margin: '10px'}} to={'/'}>Main </Link>
-            <Link to={'/about'} >About </Link>
-            <Suspense fallback={null}>
-            <Routes>
-                <Route  path={'/about'} element={<AboutPage />}/>
-                <Route  path={'/'} element={<MainPage />}/>
-            </Routes>
-            </Suspense>
+
         </div>
     );
 };
